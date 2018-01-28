492184380b7233630aebe06fd2855c2304e42377
Suresh Pokharel <suresh.wrc@gmail.com>
2018-01-28 18:15:49 +0545

    Create toogle-string.py

---
+++ toogle-string.py
@@ -0,0 +1,9 @@
+class Test:
+        
+    def toogleString(inputString):
+        print(inputString.swapcase()) # Return a copy of s, but with lower case letters converted to upper case and vice versa.
+            
+
+inputString = input()
+newObj = Test
+newObj.toogleString(inputString) 
